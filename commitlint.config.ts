b9f11542ccec0b5176fc12d090ee6525327fb68c
Kacper Lechicki <klechicki.code@gmail.com>
2024-08-26 09:08:25 +0200

    SC-0 test: initial commit

---
+++ commitlint.config.ts
@@ -0,0 +1,22 @@
+// @ts-nocheck
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-match-pattern': [2, 'always', /^SC-\d+\s(feat|fix|docs|style|refactor|test|chore):\s.+/],
+    'subject-empty': [2, 'always'],
+    'type-empty': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'header-match-pattern': ({ header }): [boolean, string] => {
+          const jiraAndTypeRegex: RegExp = /^SC-\d+\s(feat|fix|docs|style|refactor|test|chore):\s.+/;
+          return [
+            jiraAndTypeRegex.test(header),
+            'Header must contain a JIRA issue number starting with SC- followed by a type and description \n' + 'example: SC-0 test: test',
+          ];
+        },
+      },
+    },
+  ],
+};
